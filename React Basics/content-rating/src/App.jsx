b306b15faf373d670007c6c009c0abb1274a54c0
DanielaDuque <daniela_0708@outlook.com>
2025-03-19 16:52:58 +0100

    React - Lab Content rating

---
+++ React Basics/content-rating/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import ContentRating from "./components/ContentRating/ContentRating"
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <ContentRating/>
+    </>
+  )
+}
+
+export default App
