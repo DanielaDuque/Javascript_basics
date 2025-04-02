0571e3e5c346cada980c5883fe7b96bae0f97137
DanielaDuque <daniela_0708@outlook.com>
2025-04-02 16:22:48 +0200

    React Lab : Event Planner

---
+++ React Basics/EventPlanner/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import EventPlanner from './components/EventPlanner/EventPlanner'
+
+function App() {
+
+  return (
+    <>
+      <EventPlanner />
+    </>
+  )
+}
+
+export default App
